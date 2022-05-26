d97f79edac66d767b63f66ffbee7524397c43657
alejo <alejo_mareco@hotmail.com>
2022-05-26 18:50:22 -0300

    practica de operadores

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,3 +1,20 @@
 var nombreCompleto = 3;
 var apellido = 'Mareco';
 nombre = 'Alejandro Julian';
+var numero1 = 0;
+var numero2 = 0;
+var res = 0;
+res = numero1 + numero2;
+//  alert(res);
+var numero3 = 1;
+var numero4 = 2;
+var res2 = 0;
+res2 = ++numero3;
+res3 = numero4++;
+//  alert(res2);
+//  alert(res3);
+
+var num = 2;
+var res5 = 1;
+res += num;
+alert(res5);
